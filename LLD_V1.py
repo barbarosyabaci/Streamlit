8bb121cfd12da877720b73b27d4151a3958dce14
barbarosyabaci <barbaros.yabaci@gmail.com>
2024-06-07 12:10:25 +0300

    V2 Changed

--- LLD_V1.py
+++ LLD_V1.py
@@ -50,8 +50,8 @@ if selected == "LLD Database Model":
     TI_Number_to_display = st.selectbox('Please Select The TI Number', TI_numbers_list)
 
     if st.button("Display TI Number information"):
-        document = collection.find_one({"TI_Number": TI_Number_to_display}).values()
-        #vdocument = collection.find_one({"TI_Number": TI_Number_to_display})
+        # document = collection.find_one({"TI_Number": TI_Number_to_display}).values()
+        document = collection.find_one({"TI_Number": TI_Number_to_display})
         # st.table(document)
         st.write(type(document))
         # st.write(document)
