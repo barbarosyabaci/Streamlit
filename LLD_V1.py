a881b081d7e68492d6d95eefbd460fbc9ecb5d3d
barbarosyabaci <barbaros.yabaci@gmail.com>
2024-06-07 12:09:56 +0300

    V2 Changed

--- LLD_V1.py
+++ LLD_V1.py
@@ -53,7 +53,7 @@ if selected == "LLD Database Model":
         document = collection.find_one({"TI_Number": TI_Number_to_display}).values()
         #vdocument = collection.find_one({"TI_Number": TI_Number_to_display})
         # st.table(document)
-        # st.write(type(document))
+        st.write(type(document))
         # st.write(document)
 
         def display_dict(d, level=0):
@@ -70,7 +70,7 @@ if selected == "LLD Database Model":
                 st.write(d)
 
         # display_dict(document)
-        display_dict(document)
+        # display_dict(document)
 
 if selected == "CDR Reporting":
     import pandas as pd, zipfile,CDR_functions_old as cdrf
