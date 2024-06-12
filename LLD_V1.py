fb72f928d9d3bbabecb03de1218c1d847acfabdd
barbarosyabaci <barbaros.yabaci@gmail.com>
2024-06-12 15:15:33 +0300

    V2 Changed

--- LLD_V1.py
+++ LLD_V1.py
@@ -48,6 +48,15 @@ if selected == "LLD Data":
 
     TI_numbers_list = df_1["TI_Number"].to_list()
     TI_Number_to_display = st.selectbox('Please Select The TI Number', TI_numbers_list)
+    TI_Number_to_update = st.selectbox('Please Select The TI Number', TI_numbers_list)
+
+    if st.button("Update information for TI Number as JSON"):
+        document_2 = collection.find_one({"TI_Number": TI_Number_to_update}
+        # document = collection.find_one({"TI_Number": TI_Number_to_display})
+        # document = collection.find_one({"TI_Number": TI_Number_to_display}, {"_id": 0, "field_to_exclude": 0})
+        # document = collection.find_one({"TI_Number": TI_Number_to_display}, {"_id": 0, "field1": 0, "field2": 0})
+        # st.write(document)
+        # st.json(document, expanded=False)
 
     if st.button("Display TI Number information"):
         # document = collection.find_one({"TI_Number": TI_Number_to_display})
@@ -72,13 +81,7 @@ if selected == "LLD Data":
 
     st.download_button(label="Download as CSV", data=csv_string, file_name=TI_Number_to_display + '.csv', mime="text/csv")
 
-    if st.button("Update information for TI Number as JSON"):
-        a = 2
-        # document = collection.find_one({"TI_Number": TI_Number_to_display})
-        # document = collection.find_one({"TI_Number": TI_Number_to_display}, {"_id": 0, "field_to_exclude": 0})
-        # document = collection.find_one({"TI_Number": TI_Number_to_display}, {"_id": 0, "field1": 0, "field2": 0})
-        # st.write(document)
-        # st.json(document, expanded=False)
+
 
 
 
